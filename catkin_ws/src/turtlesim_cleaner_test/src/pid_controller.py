a5d86f3cfed006d9c5b73390df56786bdeb0b7c9
prithvirajg <prithviraj.gotepatil19@vit.edu>
2023-07-14 16:42:43 +0530

    added a video

--- catkin_ws/src/turtlesim_cleaner_test/src/pid_controller.py
+++ catkin_ws/src/turtlesim_cleaner_test/src/pid_controller.py
@@ -2,6 +2,7 @@
 
 class PID:
     def __init__(self, P=2.0, I=0.0, D=1.0, derivative=0, integral=0, integral_max=500, integral_min=-500):
+        # initialize variables
         self.Kp = P
         self.Ki = I
         self.Kd = D
@@ -16,17 +17,22 @@ class PID:
     def update(self, error):
         self.error = error
 
+        # calculate P valuebased on error
         self.P_value = self.Kp * self.error
+
+        # calculate the D value based on derivative
         self.D_value = self.Kd * (self.error - self.derivative)
         self.derivative = self.error
 
         self.integral = self.integral + self.error
 
+        # limit the integral term
         if self.integral > self.integral_max:
             self.integral = self.integral_max
         elif self.integral < self.integral_min:
             self.integral = self.integral_min
 
+        # calculate I value based on integral
         self.I_value = self.integral * self.Ki
 
         PID = self.P_value + self.I_value + self.D_value
